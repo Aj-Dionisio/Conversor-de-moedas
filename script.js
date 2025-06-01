d1cd36a8c5079eb45b1be27217f798f18750d55e
Antonio <jorge_11000@hotmail.com>
2025-06-01 18:13:49 -0300

    modificações nos arquivos, edição do valor apresentado abaixo das bandeiras

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 const convertButton = document.getElementById('ButtonConvert');
 
+function converterValores(){
+    const inputValue = document.getElementById('valor-converter').value
+    const CurrencyValueToConvert = document.querySelector('.currency-value-to-convert')// mapeia o elemento HTML onde o valor a ser convertido será exibido 
+
+    const CurrencyValueConverted = document.querySelector('.currency-value')// mapeia o elemento HTML onde o valor convertido será exibido
 
 
-function converterCurrency(){
-    const inputValue = document.getElementById('valor-converter').value;
+    //Exemplos de taxa de conversão
+    const dolarToday = 5.25;
+    const euroToday = 5.60;
+    const bitcoinToday = 120000;
+    const realToday = 1;
+    const libraToday = 0.86;
+    const ieneToday = 0.0077;
+    const pesoToday = 0.018;
 
-    const dolarToday = 5.25;     // Example conversion rate
-    const euroToday = 5.60;      // Example conversion rate
-    const bitcoinToday = 300000; // Example conversion rate
-    const realToday = 1;         // Example conversion rate
-    const libraToday = 6.00;      // Example conversion rate
-    const ieneToday = 0.04;       // Example conversion rate
-    const pesoToday = 0.05;      // Example conversion rate
 
-    const convertedValue = inputValue/dolarToday;
+    const convertedValue = inputValue/dolarToday
 
-    console.log(`Converted value in dollars: ${convertedValue}`);
+   CurrencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style:"currency",
+    currency:"BRL",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 3,
+   }).format(inputValue) 
+   // o valor a ser convertido será apresentado abaixo da bandeira do Brasil
+    CurrencyValueConverted.innerHTML = new Intl.NumberFormat ("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 3,
+    }).format(convertedValue) // o valor convertido será apresentado abaixo da bandeira dos Estados Unidos
+
+
+    // console.log(convertedValue);
 }
 
-convertButton.addEventListener('click',converterCurrency);
+convertButton.addEventListener('click', converterValores);
