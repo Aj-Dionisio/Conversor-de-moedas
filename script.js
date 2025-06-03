1183ca202ac2957365023f2aaff741cc7583b933
Antonio <jorge_11000@hotmail.com>
2025-06-02 22:05:08 -0300

    converções funcionando

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const convertButton = document.getElementById('ButtonConvert');
 const currencyToConvert = document.querySelector('.currency-to-convert');// mapeia o elemento HTML onde a moeda a ser convertida será selecionada
 const currencySelect = document.querySelector('.currency-converted');// mapeia o elemento HTML onde a moeda convertida será selecionada
 
-function converterValores(){
-        const inputValue = parseFloat(document.getElementById('valor-converter').value);// adição do parseFloat para garantir que o valor seja um número decimal
+function converterValores() {
+    const inputValue = parseFloat(document.getElementById('valor-converter').value);// adição do parseFloat para garantir que o valor seja um número decimal
     const CurrencyValueToConvert = document.querySelector('.currency-value-to-convert')// mapeia o elemento HTML onde o valor a ser convertido será exibido 
 
     const CurrencyValueConverted = document.querySelector('.currency-value')// mapeia o elemento HTML onde o valor convertido será exibido
 
-    console.log(currencyToConvert.value) // exibe no console o valor da moeda a ser convertida;
-    console.log(currencySelect.value)// exibe no console o valor da moeda convertida;
+    // console.log(currencyToConvert.value) // exibe no console o valor da moeda a ser convertida;
+    // console.log(currencySelect.value)// exibe no console o valor da moeda convertida;
 
     //Exemplos de taxa de conversão
     const dolarToday = 5.25;
@@ -26,7 +26,7 @@ function converterValores(){
             currency: "USD",
             minimumFractionDigits: 2,
             maximumFractionDigits: 3,
-        }).format(inputValue/dolarToday) // o valor convertido será apresentado abaixo da bandeira dos Estados Unidos
+        }).format(inputValue / dolarToday) // o valor convertido será apresentado abaixo da bandeira dos Estados Unidos
     }
     if (currencySelect.value === "Euro") {
         CurrencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
@@ -34,8 +34,8 @@ function converterValores(){
             currency: "EUR",
             minimumFractionDigits: 2,
             maximumFractionDigits: 3,
-        }).format(inputValue/euroToday) // o valor convertido será apresentado abaixo da bandeira da Europa
-        
+        }).format(inputValue / euroToday) // o valor convertido será apresentado abaixo da bandeira da Europa
+
     }
     if (currencySelect.value === "Bitcoin") {
         CurrencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
@@ -43,7 +43,7 @@ function converterValores(){
             currency: "BTC",
             minimumFractionDigits: 2,
             maximumFractionDigits: 3,
-        }).format(inputValue/bitcoinToday) // o valor convertido será apresentado abaixo da bandeira do Bitcoin
+        }).format(inputValue / bitcoinToday) // o valor convertido será apresentado abaixo da bandeira do Bitcoin
     }
     if (currencySelect.value === "Real") {
         CurrencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
@@ -51,7 +51,7 @@ function converterValores(){
             currency: "BRL",
             minimumFractionDigits: 2,
             maximumFractionDigits: 3,
-        }).format(inputValue/realToday) // o valor convertido será apresentado abaixo da bandeira do Brasil
+        }).format(inputValue / realToday) // o valor convertido será apresentado abaixo da bandeira do Brasil
     }
     if (currencySelect.value === "Libra") {
         CurrencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
@@ -59,7 +59,7 @@ function converterValores(){
             currency: "GBP",
             minimumFractionDigits: 2,
             maximumFractionDigits: 3,
-        }).format(inputValue/libraToday) // o valor convertido será apresentado abaixo da bandeira do Reino Unido
+        }).format(inputValue / libraToday) // o valor convertido será apresentado abaixo da bandeira do Reino Unido
     }
     if (currencySelect.value === "Iene") {
         CurrencyValueConverted.innerHTML = new Intl.NumberFormat("ja-JP", {
@@ -67,7 +67,7 @@ function converterValores(){
             currency: "JPY",
             minimumFractionDigits: 2,
             maximumFractionDigits: 3,
-        }).format(inputValue/ieneToday) // o valor convertido será apresentado abaixo da bandeira do Japão
+        }).format(inputValue / ieneToday) // o valor convertido será apresentado abaixo da bandeira do Japão
     }
     if (currencySelect.value === "Peso") {
         CurrencyValueConverted.innerHTML = new Intl.NumberFormat("es-AR", {
@@ -75,23 +75,58 @@ function converterValores(){
             currency: "ARS",
             minimumFractionDigits: 2,
             maximumFractionDigits: 3,
-        }).format(inputValue/pesoToday) // o valor convertido será apresentado abaixo da bandeira da Argentina
+        }).format(inputValue / pesoToday) // o valor convertido será apresentado abaixo da bandeira da Argentina
     }
-    
 
 
-   CurrencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-    style:"currency",
-    currency:"BRL",
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 3,
-   }).format(inputValue) 
-   // o valor a ser convertido será apresentado abaixo da bandeira do Brasil
 
+    CurrencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 3,
+    }).format(inputValue)
+    // o valor a ser convertido será apresentado abaixo da bandeira do Brasil
 
-    // console.log(convertedValue);
 }
 
-convertButton.addEventListener('click', converterValores);
+function changecurrency(){
+    const currencyName = document.getElementById('currency-name');
+    const currencyFlag = document.getElementById('currency-flag');
+
+    if (currencySelect.value === "Dollar") {
+        currencyName.innerHTML = "Dólar";
+        currencyFlag.src = "./Assets/Dollar.png"; // URL da bandeira dos Estados Unidos
+
+    }
+    if (currencySelect.value === "Euro") {
+        currencyName.innerHTML = "Euro";
+        currencyFlag.src = "./Assets/Euro.png"; // URL da bandeira do Euro
+
+    }
+    // if (currencySelect.value === "Bitcoin") {
+    //     currencyName.innerHTML = "Bitcoin";
+    // }
+    // if (currencySelect.value === "Real") {
+    //     currencyName.innerHTML = "Real";
+    // }
+    // if (currencySelect.value === "Libra") {
+    //     currencyName.innerHTML = "Libra";
+    // }
+    // if (currencySelect.value === "Iene") {
+    //     currencyName.innerHTML = "Iene";
+    // }
+    // if (currencySelect.value === "Peso") {
+    //     currencyName.innerHTML = "Peso";
+    // }
+    converterValores(); // Chama a função para atualizar o valor convertido ao mudar a moeda selecionada
+
+}
+    currencySelect.addEventListener('change', changecurrency);
+    currencyToConvert.addEventListener('change', changecurrency);
 
 
+    convertButton.addEventListener('click', converterValores);
+
+//adicionar outras moedas e fazer a conversão de acordo com a moeda selecionada;
+//adicionar a bandeira de cada moeda selecionada;
