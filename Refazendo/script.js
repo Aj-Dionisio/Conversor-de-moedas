1ac719a0e6dfd3d55e6ce7fbe3f28accbe2715cc
Antonio <jorge_11000@hotmail.com>
2025-08-12 22:10:28 -0300

    INICIO DO JAVASCRIPT

---
+++ Refazendo/script.js
@@ -0,0 +1,58 @@
+const input = document.querySelector('.currencyValue')
+const paragraph = document.querySelector('.valueCurrencyConvert')
+const valueConverted = document.querySelector('.valueConverted')
+const image = document.querySelector('#flagConvert')
+const imageConverted = document.querySelector('#flagConverted')
+const currencySelectConvert = document.querySelector('#currencyConvert')
+const currencySelectConverted = document.querySelector('#currencyConverted')
+
+const CURRENCY_VALUE = {
+    DOLLAR: 5.43,
+    EURO: 6.33,
+}
+
+const euroConta = input.value / CURRENCY_VALUE.EURO
+
+
+function conta() {
+    paragraph.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(input.value)
+
+    const dollarConta = input.value / CURRENCY_VALUE.DOLLAR
+
+    if (currencySelectConvert.value == 'dollar') {
+        valueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(dollarConta)
+
+    }
+
+
+}
+
+function changeCurrencyConvert() {
+    if () {
+        image.innerHTML = image.src = "./Assets/Euro.png"
+    } else if () {
+        image.innerHTML = image.src = "./Assets/Euro.png"
+    } else {
+        image.innerHTML = image.src = "./Assets/Euro.png"
+    }
+
+}
+
+function changeConvertedCurrency() {
+    if (currencySelectConvert.value === 'dollar') {
+        image.innerHTML = imageConverted.src = "./Assets/Real.png"
+    } else if (currencySelectConvert.value === 'euro') {
+        image.innerHTML = imageConverted.src = "./Assets/Euro.png"
+
+    } else {
+        image.innerHTML = imageConverted.src = "./Assets/Real.png"
+    }
+
+}
+
